c3421ea52e04f4ebeeafa8a20b2e1f934f397ab1
JrgenGNielsen <jgn@dbc.dk>
2013-03-07 12:25:12 +0100

    replace function for user menu: change logout to login

--- js/user_timeout.js
+++ js/user_timeout.js
@@ -1,27 +1,12 @@
-/*
-(function ($) {
-  Drupal.behaviors.bibdk_timeout = {
-    attach:function (context) {
-      $.ajax({
-        url: '/ajax/bibdk_timeout',
-        success: function(msg) {
-          // alert(msg);
-          // $('#messages').html('<div class="messages status"><h2 class="element-invisible">Status message</h2>' + msg + '</div>');
-        }
-      });
-    },
-    detach:function (context) {
-    }
-  };
-}(jQuery));
-*/
-
 (function ($) {
   Drupal.behaviors.bibdk_timeout = {
     attach:function (context) {
       var msg = Drupal.t('Your session has timed out.');
       alert(msg);
       $('#messages').html('<div class="messages status"><h2 class="element-invisible">Status message</h2>' + msg + '</div>');
+      $('a[href$="/user/logout"]').html(Drupal.t('Log in'));
+      var value = $('a[href$="/user/logout"]').attr('href').replace("/logout", "/login");
+      $('a[href$="/user/logout"]').attr('href',value);
     },
     detach:function (context) {
     }
